09efb9a35792def59f528743405edfd677f28250
AidenScottStich <stichaiden2@gmail.com>
2024-04-24 09:21:31 -0500

    Initial Commit

---
+++ PokemonAPI/PokemonAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAPIApp.swift
+//  PokemonAPI
+//
+//  Created by STICH, AIDEN S. on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
