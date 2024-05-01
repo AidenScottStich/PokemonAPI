ac3a1fa80658b7933fb786fe0a6f50c7dd81f8b7
AidenScottStich <stichaiden2@gmail.com>
2024-05-01 08:44:19 -0500

    tabview

---
+++ PokemonAPI/PokemonCard.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonCard.swift
+//  PokemonAPI
+//
+//  Created by STICH, AIDEN S. on 4/29/24.
+//
+
+import SwiftUI
+
+struct PokemonCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonCard()
+}
