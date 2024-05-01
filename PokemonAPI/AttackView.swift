131d00103cd431f1d1baf96133941e8f46f16954
AidenScottStich <stichaiden2@gmail.com>
2024-05-01 10:41:50 -0500

    Attack View

---
+++ PokemonAPI/AttackView.swift
@@ -0,0 +1,18 @@
+//
+//  AttackView.swift
+//  PokemonAPI
+//
+//  Created by STICH, AIDEN S. on 5/1/24.
+//
+
+import SwiftUI
+
+struct AttackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttackView()
+}
