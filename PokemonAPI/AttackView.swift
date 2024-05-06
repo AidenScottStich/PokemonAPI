3726f87d25832e8d23e15f330562b3f919fa043e
AidenScottStich <stichaiden2@gmail.com>
2024-05-06 09:10:45 -0500

    attack background

--- PokemonAPI/AttackView.swift
+++ PokemonAPI/AttackView.swift
@@ -8,104 +8,113 @@ struct AttackView: View {
     
 
     var body: some View {
-        VStack {
-            if let pokemon1 = pokemon1, let pokemon2 = pokemon2 {
-                
-                Text(pokemon1.name)
-                
-                AsyncImage(url: URL(string: pokemon1.sprites.front_default)) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView()
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    case .failure(let error):
-                        Text("Failed to load image: \(error.localizedDescription)")
-                    @unknown default:
-                        EmptyView()
+        
+        ZStack{
+            Image("Grass") // Set your custom image name her
+                .resizable()
+                .aspectRatio(contentMode: .fill)              .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                if let pokemon1 = pokemon1, let pokemon2 = pokemon2 {
+                    
+                    Text(pokemon1.name)
+                    
+                    AsyncImage(url: URL(string: pokemon1.sprites.front_default)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        case .failure(let error):
+                            Text("Failed to load image: \(error.localizedDescription)")
+                        @unknown default:
+                            EmptyView()
+                        }
                     }
-                }
-                .frame(width: 250, height: 250)
-                
-                Text(pokemon2.name)
-                
-                AsyncImage(url: URL(string: pokemon2.sprites.front_default)) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView()
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    case .failure(let error):
-                        Text("Failed to load image: \(error.localizedDescription)")
-                    @unknown default:
-                        EmptyView()
+                    .frame(width: 250, height: 250)
+                    
+                    Text(pokemon2.name)
+                    
+                    AsyncImage(url: URL(string: pokemon2.sprites.front_default)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        case .failure(let error):
+                            Text("Failed to load image: \(error.localizedDescription)")
+                        @unknown default:
+                            EmptyView()
+                        }
                     }
-                }
-                .frame(width: 250, height: 250)
+                    .frame(width: 250, height: 250)
 
-                
-                Button(action: {
-                    battle()
-                }) {
-                    Text("Battle!")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding()
-                
-                if let winner = winner {
-                    Text("Winner: \(winner.name)")
-                        .font(.headline)
-                        .padding()
-                }
-            } else {
-                ProgressView()
+                    
+                    Button(action: {
+                        battle()
+                    }) {
+                        Text("Battle!")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
                     .padding()
+                    
+                    if let winner = winner {
+                        Text("Winner: \(winner.name)")
+                            .font(.headline)
+                            .padding()
+                    }
+                } else {
+                    ProgressView()
+                        .padding()
+                }
             }
-        }
-        .onAppear {
-            fetchRandomPokemons()
+            .onAppear {
+                fetchRandomPokemons()
+            }
+            
+            .sheet(isPresented: $showResult) {
+                if let winner = winner {
+                    
+                    Text("\(winner.name) wins!")
+                    
+                    AsyncImage(url: URL(string: winner.sprites.front_default)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        case .failure(let error):
+                            Text("Failed to load image: \(error.localizedDescription)")
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                    .frame(width: 300, height: 300)
+                    
+                    Button(action: {
+                                    resetPage()
+                                }) {
+                                    Text("Reset")
+                                        .padding()
+                                        .background(Color.red)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                }
+                                .padding()
+                }
+            }
         }
         
-        .sheet(isPresented: $showResult) {
-            if let winner = winner {
-                
-                Text("\(winner.name) wins!")
-                
-                AsyncImage(url: URL(string: winner.sprites.front_default)) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView()
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    case .failure(let error):
-                        Text("Failed to load image: \(error.localizedDescription)")
-                    @unknown default:
-                        EmptyView()
-                    }
-                }
-                .frame(width: 300, height: 300)
-                
-                Button(action: {
-                                resetPage()
-                            }) {
-                                Text("Reset")
-                                    .padding()
-                                    .background(Color.red)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(10)
-                            }
-                            .padding()
-            }
-        }
+
     }
     
     func fetchRandomPokemons() {
