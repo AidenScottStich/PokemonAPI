637ab85e1789856c4a7338266149ce3daa16869b
Johnny <johsun240@gmail.com>
2024-05-06 10:10:52 -0500

    change pokemon card layout a little

--- PokemonAPI/PokemonCard.swift
+++ PokemonAPI/PokemonCard.swift
@@ -11,57 +11,58 @@ struct PokemonCard: View {
     @Environment(\.dismiss) var dismiss
     let pokemon: ApiResponse
     var body: some View {
-        
-        VStack{
-            ScrollView{
-                Text(pokemon.name.capitalized)
-                    .font(.largeTitle)
-                    .padding(.top, 20)
-
-                
-                let typeNames = pokemon.types.map { $0.type.name.capitalized }.joined(separator: ", ")
-                Text("Type: \(typeNames)")
-                
-                AsyncImage(url: URL(string:pokemon.sprites.front_default)) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView() // Placeholder while loading
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    case .failure(let error):
-                        Text("Failed to load image: \(error.localizedDescription)")
-                    @unknown default:
-                        EmptyView()
-                    }
-                }
-                .frame(width: 300, height: 300)
-                
-                VStack(alignment: .leading){
-                    Text("Base Stats")
-
-                        .bold()
-                        .padding([.bottom], 5)
-                    ForEach(pokemon.stats ?? []){ stat in
-                        Text(" \(stat.stat.name.capitalized) \(stat.base_stat)")
-                        
-                    }
-                }.padding([.bottom],10)
+        ZStack{
+            VStack{
+                ScrollView{
+                    Text(pokemon.name.capitalized)
+                        .font(.largeTitle)
+                        .padding(.top, 60)
                     
-                Text("Weight: \(pokemon.weight)")
-                VStack{
-                    Text("Avliable Moves")
-                        .bold()
-                        .padding([.top], 5)
-                        .padding([.bottom], 5)
-                    ForEach(pokemon.moves ?? []) { move in
-                        Text(move.move.name.capitalized)
+                    
+                    let typeNames = pokemon.types.map { $0.type.name.capitalized }.joined(separator: ", ")
+                    Text("Type: \(typeNames)")
+                    
+                    AsyncImage(url: URL(string:pokemon.sprites.front_default)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView() // Placeholder while loading
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        case .failure(let error):
+                            Text("Failed to load image: \(error.localizedDescription)")
+                        @unknown default:
+                            EmptyView()
+                        }
                     }
+                    .frame(width: 300, height: 300)
+                    
+                    VStack(alignment: .leading){
+                        Text("Base Stats")
+                        
+                            .bold()
+                            .padding([.bottom], 5)
+                        ForEach(pokemon.stats ?? []){ stat in
+                            Text(" \(stat.stat.name.capitalized) \(stat.base_stat)")
+                            
+                        }
+                    }.padding([.bottom],10)
+                    
+                    Text("Weight: \(pokemon.weight)")
+                    VStack{
+                        Text("Avliable Moves")
+                            .bold()
+                            .padding([.top], 5)
+                            .padding([.bottom], 5)
+                        ForEach(pokemon.moves ?? []) { move in
+                            Text(move.move.name.capitalized)
+                        }
+                    }
+                    
                 }
                 
             }
-     
      
             
             Button(action: {
@@ -75,6 +76,7 @@ struct PokemonCard: View {
                     .background(Color.black)
                     .cornerRadius(10)
                     .padding(50)
+                    .padding([.top],700)
             }
         }.background(Color.blue)
             .overlay(
